8ad8ef080275bd31f9553dca5631c5961dd5baab
RasPiBot <Shany.Carle@gmail.com>
2020-04-16 10:59:39 -0400

    Avec publication du build

---
+++ projet2v2/build/precache-manifest.cb2b4465283d4b573973626f650e5bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6129d847de4e8f9331855978686c43ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18c63423965250661394",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c972be523d9e6dc79aa7",
+    "url": "/static/js/2.73933c58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73933c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c63423965250661394",
+    "url": "/static/js/main.4c533004.chunk.js"
+  },
+  {
+    "revision": "a36de3c8a723ddc202d9",
+    "url": "/static/js/runtime-main.ee736eb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
