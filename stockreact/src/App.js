d3eda7ec415f836f55e6c8c86f700d1077b14ba9
RasPiBot <Shany.Carle@gmail.com>
2020-04-23 13:55:06 -0400

    Ajout de Bootstrap

--- stockreact/src/App.js
+++ stockreact/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs lg="2">
+          1 of 3
+        </Col>
+        <Col md="auto">Variable width content</Col>
+        <Col xs lg="2">
+          3 of 3
+        </Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col md="auto">Variable width content</Col>
+        <Col xs lg="2">
+          3 of 3
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
