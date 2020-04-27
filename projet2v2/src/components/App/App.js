06a12c3ac613fdb2f0ca83621e1c5946718f3f97
RasPiBot <Shany.Carle@gmail.com>
2020-04-27 16:06:06 -0400

    Avec Components dans le démo

--- projet2v2/src/components/App/App.js
+++ projet2v2/src/components/App/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../../img/logo.svg";
+import MonPremierComponent from "../MonPremierComponent/MonPremierComponent";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MonPremierComponent />
+        <MonPremierComponent />
+        <MonPremierComponent />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ projet2v2/src/components/MonPremierComponent/MonPremierComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MonPremierComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>BOO YA</h1>
+        <p>Un component</p>
+      </div>
+    );
+  }
+}
+
+export default MonPremierComponent;
