48bfa55b808100368b9dcb0c269bffd1b600c1b5
RasPiBot <Shany.Carle@gmail.com>
2020-05-19 20:11:06 -0400

    modification a deconnexion

--- becflixV3/src/components/Deconnexion/Deconnexion.js
+++ becflixV3/src/components/Deconnexion/Deconnexion.js
@@ -2,14 +2,14 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 
 export class Deconnexion extends React.Component {
-    handleDeconnexion(){
-        this.props.onClick(false);
-    }
-    render() {
+  handleDeconnexion() {
+    this.props.onClick(false);
+  }
+  render() {
     return (
-        <Button variant="primary" onClick={this.handleDeconnexion()}>
-            Déconnexion
-        </Button>
+      <Button variant="primary" onClick={() => this.handleDeconnexion()}>
+        Déconnexion
+      </Button>
     );
   }
 }
