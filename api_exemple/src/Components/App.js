fc2befe880e96c50cd50611d8ffa33a0acd5b45a
SLAY666 <Shany.Carle@gmail.com>
2020-06-23 21:51:01 -0400

    Version 2 de ShortenURL

--- api_exemple/src/Components/App.js
+++ api_exemple/src/Components/App.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import React from 'react';
 import {ShortenURL} from './ShortenURL';
-=======
-import React from "react";
-import { PersonnageV4 } from "./PersonnageV4";
->>>>>>> ee24c1bbc2030868a551b8570dbf7b6119b77d52
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <ShortenURL></ShortenURL>
-=======
-      <PersonnageV4 />
->>>>>>> ee24c1bbc2030868a551b8570dbf7b6119b77d52
     </>
   );
 }
--- api_exemple/src/Components/ShortenURL.js
+++ api_exemple/src/Components/ShortenURL.js
@@ -1,41 +1,42 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 export class ShortenURL extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {longURL : "https://www.youtube.com/watch?v=3VmtibKpmXI", shortURL : ""};
+    this.state = {shortURL : "", longURL : "https://www.youtube.com/watch?v=3VmtibKpmXI"};
+    this.getData = this.getData.bind(this);  
   }
 
-  componentDidMount(){
-const apiKey = "VOTRE API"; 
-const getData = async () => { 
-   try{ 
-    console.log("try");
-      const response = await fetch('https://api.rebrandly.com/v1/links', { 
-         method:'POST', 
-         headers: {'Content-Type': 'application/json','apikey': apiKey  }, 
-         body:JSON.stringify({destination: this.state.longURL }) 
-       }); 
-      if(response.ok){ 
-         const jsonResponse = await response.json(); 
-         console.log(jsonResponse);
-         this.setState({shortURL : jsonResponse.shortUrl});
-         return jsonResponse; 
+  async getData(){ 
+      const apiKey = "35768e03791444ae8db11e881d18a92e"; 
+      try{ 
+          const response = await fetch('https://api.rebrandly.com/v1/links', { 
+            method:'POST', 
+            headers: {'Content-Type': 'application/json','apikey': apiKey  }, 
+            body:JSON.stringify({destination:  this.state.longURL}) 
+          }); 
+          if(response.ok){ 
+            const jsonResponse = await response.json(); 
+            console.log(jsonResponse);
+            this.setState({shortURL : jsonResponse.shortUrl});
+            return jsonResponse; 
+          } 
+          throw new Error('Request failed!'); 
       } 
-      throw new Error('Request failed!'); 
-   } 
-   catch(error){ 
-      console.log(error); 
-   } 
+      catch(error){ 
+          console.log(error); 
+      } 
 } 
-getData();
-  }
-
+  
+  
   render() {
     return (
       <>
       <h1>rebrandly</h1>
       <h2>{this.state.shortURL}</h2>
+      <Button onClick={this.getData}>Call API</Button>
+
       </>
     );
   }
