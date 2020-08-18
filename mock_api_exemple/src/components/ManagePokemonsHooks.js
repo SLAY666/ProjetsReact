7fd21be1bc730f2e7d2b5473c229f7d3c149143e
RasPiBot <Shany.Carle@gmail.com>
2020-08-18 14:20:29 -0400

    test build

--- mock_api_exemple/src/components/ManagePokemonsHooks.js
+++ mock_api_exemple/src/components/ManagePokemonsHooks.js
@@ -1,6 +1,5 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Pokemon } from "./Pokemon";
-
 
 function ManagePokemonsHooks() {
   const [donneesRecues, setDonneesRecues] = useState([]);
@@ -8,29 +7,29 @@ function ManagePokemonsHooks() {
   //Ajout de la gestion des erreurs
   useEffect(() => {
     getPokemons();
-    },donneesRecues); //Si on enlève le second paramètre, on obtient une boucle infinie.
-    
-    async function getPokemons(){
-      try {
-        const response =  await fetch("http://localhost:3001/pokemons");
-        const reponseDeApi =  await response.json();
-        setDonneesRecues(reponseDeApi);
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
+  }, donneesRecues); //Si on enlève le second paramètre, on obtient une boucle infinie.
 
-    return (
-      <div>
-        <h1>Affichage de la liste de tous les Pokemons</h1>
-        {donneesRecues.map((key,i) => (
-          <Pokemon nom={key.name} id={key.id} key={key.name+key.id}></Pokemon>
-        ))}
-      </div>
-    );
+  async function getPokemons() {
+    try {
+      const response = await fetch("http://localhost:3001/pokemons");
+      const reponseDeApi = await response.json();
+      setDonneesRecues(reponseDeApi);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Affichage de la liste de tous les Pokemons</h1>
+      {donneesRecues.map((key, i) => (
+        <Pokemon nom={key.name} id={key.id} key={key.name + key.id}></Pokemon>
+      ))}
+    </div>
+  );
 }
 
 export default ManagePokemonsHooks;
