3f07f44fe475f88ef2910555fa62bd28ff4a7c81
SLAY666 <Shany.Carle@gmail.com>
2020-08-25 10:23:10 -0400

    premier PWA

---
+++ localdbprojectpwa/build/precache-manifest.0a1a7761f91d3573b823ed8cfcfdabc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29178f024bd58e9aa85bb4ff6679c459",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e6ea0d557b5239b460f",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bda11bef1b82ba816e36",
+    "url": "./static/js/2.ae539a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ae539a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6ea0d557b5239b460f",
+    "url": "./static/js/main.1d894cac.chunk.js"
+  },
+  {
+    "revision": "bb4922d3edace0c405c7",
+    "url": "./static/js/runtime-main.11fb3d3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- localdbprojectpwa/build/service-worker.js
+++ localdbprojectpwa/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3c48e8188ef621bd5d17ec43da22c89.js"
+  "./precache-manifest.0a1a7761f91d3573b823ed8cfcfdabc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
