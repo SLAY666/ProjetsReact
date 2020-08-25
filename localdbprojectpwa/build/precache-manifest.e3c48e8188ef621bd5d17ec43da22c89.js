7cc42f0dd5d71b631e8022261999ac203852f245
SLAY666 <Shany.Carle@gmail.com>
2020-08-25 10:21:08 -0400

    premier PWA

---
+++ localdbprojectpwa/build/precache-manifest.e3c48e8188ef621bd5d17ec43da22c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735903413787d7ed5d94fb314cc73fc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1219b1ade80460378b98",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bda11bef1b82ba816e36",
+    "url": "/static/js/2.ae539a19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae539a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1219b1ade80460378b98",
+    "url": "/static/js/main.b28162c1.chunk.js"
+  },
+  {
+    "revision": "e76a2379e1a86fcf5377",
+    "url": "/static/js/runtime-main.59c643d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
