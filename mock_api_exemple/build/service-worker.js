6725365e1f8bb89d31c9c53ad7fb6e5fa6f8c8c2
SLAY666 <Shany.Carle@gmail.com>
2020-09-08 18:31:51 -0400

    version démo

--- mock_api_exemple/build/service-worker.js
+++ mock_api_exemple/build/service-worker.js
@@ -23,6 +23,23 @@ self.addEventListener('message', (event) => {
   }
 });
 
+self.addEventListener('fetch', function(e) {
+  console.log('[demoPWA - ServiceWorker] Fetch event fired.', e.request.url);
+  e.respondWith(
+      caches.match(e.request).then(function(response) {
+          if (response) {
+              console.log('[demoPWA - ServiceWorker] Retrieving from cache...');
+              return response;
+          }
+          console.log('[demoPWA - ServiceWorker] Retrieving from URL...');
+          return fetch(e.request).catch(function (e) {
+             console.log('Vous êtes déconnectés...');
+          });
+      })
+  );
+});
+
+
 workbox.core.clientsClaim();
 
 /**
