14a2b2ec6b1602a3a727d849bf9b6ee1e78e9413
SLAY666 <Shany.Carle@gmail.com>
2020-09-15 18:56:42 -0400

    ajout de la validation du formulaire d'ajouts

--- mock_api_exemple/src/components/FormAjouterPokemon.js
+++ mock_api_exemple/src/components/FormAjouterPokemon.js
@@ -7,7 +7,7 @@ import {toast} from "react-toastify";
 export class FormAjouterPokemon extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {photo: "", setErrors : {}};
+    this.state = {photo: "", errors : {}};
 
     this.handleAdd = this.handleAdd.bind(this);
     this.handlePhoto = this.handlePhoto.bind(this);
@@ -46,6 +46,17 @@ export class FormAjouterPokemon extends React.Component {
    } 
 }
 
+formulaireEstValide(nom,photos,attaque1,attaque2){
+  const _errors ={};
+
+  if(!nom) _errors.nom = "Le nom du pokémon est obligatoire";
+  if(!photos) _errors.photos = "La photo du pokémon est obligatoire";
+  if(!attaque1) _errors.attaque1 = "L'attaque1 du pokémon est obligatoire";
+  if(!attaque2) _errors.attaque2 = "L'attaque2' du pokémon est obligatoire";
+
+  this.setState({errors : _errors});
+  return Object.keys(_errors).length === 0;
+}
 
   handleAdd(event){
     event.preventDefault();
@@ -55,7 +66,10 @@ export class FormAjouterPokemon extends React.Component {
     const attaque1 = document.getElementById('attaque1').value;
     const attaque2 = document.getElementById('attaque2').value;
 
-    this.addPokemon(nom,photo,attaque1,attaque2);
+    if(this.formulaireEstValide(nom,photo,attaque1,attaque2)){
+      this.addPokemon(nom,photo,attaque1,attaque2);
+    }
+    
   }
 
   handlePhoto(event){
@@ -74,20 +88,35 @@ export class FormAjouterPokemon extends React.Component {
             <Form>
               <Form.Group controlId="nomPokemon">
                 <Form.Label>Nom du Pokemon</Form.Label>
-                <Form.Control type="text" placeholder="Entrer le nom du Pokemon" />
+                <Form.Control type="text" placeholder="Entrer le nom du Pokemon" isInvalid={!!this.state.errors.nom}/>
+                <Form.Control.Feedback type="invalid">
+                  {this.state.errors.nom}
+                </Form.Control.Feedback>
               </Form.Group>
+              
               <Form.Group controlId="photoPokemon">
                 <Form.Label>URL d'une photo du Pokemon</Form.Label>
-                <Form.Control type="text" placeholder="Entrer une URL valide" onBlur={this.handlePhoto}/>
+                <Form.Control type="text" placeholder="Entrer une URL valide" onBlur={this.handlePhoto} isInvalid={!!this.state.errors.photos}/>
+                <Form.Control.Feedback type="invalid">
+                  {this.state.errors.photos}
+                </Form.Control.Feedback>
               </Form.Group>
               {this.state.photo !== "" && <Image src={this.state.photo} rounded width="125"/>}
+             
               <Form.Group controlId="attaque1">
                 <Form.Label>Nom de l'attaque 1</Form.Label>
-                <Form.Control type="text" placeholder="Entrer le nom de l'attaque 1" />
+                <Form.Control type="text" placeholder="Entrer le nom de l'attaque 1" isInvalid={!!this.state.errors.attaque1}/>
+                <Form.Control.Feedback type="invalid">
+                  {this.state.errors.attaque1}
+                </Form.Control.Feedback>
               </Form.Group>
+             
               <Form.Group controlId="attaque2">
                 <Form.Label>Nom de l'attaque 2</Form.Label>
-                <Form.Control type="text" placeholder="Entrer le nom de l'attaque 2" />
+                <Form.Control type="text" placeholder="Entrer le nom de l'attaque 2" isInvalid={!!this.state.errors.attaque2}/>
+                <Form.Control.Feedback type="invalid">
+                  {this.state.errors.attaque2}
+                </Form.Control.Feedback>
               </Form.Group>
 
             <Button variant="primary" type="submit" onClick={this.handleAdd}>
